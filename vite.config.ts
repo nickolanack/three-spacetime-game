5068c3852be1db1e7b7d88800844873501daad56
nickolanack <nickblackwell82@gmail.com>
2025-07-24 07:52:16 -0700

    config

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
+
+export default defineConfig({
+  plugins: [
+    
+    // obfuscatorPlugin({
+    
+    // }), 
+    
+    // viteSingleFile()
+    
+    ],
+ 
+  build: {
+    target: 'esnext',
+    assetsInlineLimit: Infinity,
+  },
+});
