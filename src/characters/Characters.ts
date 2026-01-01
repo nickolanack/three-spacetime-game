dd87ad20f749583ae202463a02df0059ece36885
nickolanack <nickblackwell82@gmail.com>
2026-01-01 14:04:08 -0800

    character factory methods

--- src/characters/Characters.ts
+++ src/characters/Characters.ts
@@ -1,37 +1,63 @@
+import type { CharacterController } from "../CharacterController";
+import { Character } from "./Character";
 import { Gnome } from "./Gnome"
 import { Golem } from "./Golem"
+import { Pumpkin } from "./Pumpkin";
 
 
-export type Character={
-    object:any;
-    lookAt:(object:any)=>void,
-    name?:string
-}
 
-export class Characters{
-    
+export class Characters {
+
     camera;
     renderer;
+    controllers = [];
 
-    constructor(camera, renderer){
-        this.camera=camera;
-        this.renderer=renderer;
+    constructor(camera, renderer) {
+        this.camera = camera;
+        this.renderer = renderer;
     }
 
-    createCharacter(type):Character{
+    createCharacter(type): Character {
 
-        if(type=='gnome'){
+        const character = this._loadCharacter(type)
+        character.object.character = character;
+        return character;
+
+    }
+
+    _loadCharacter(type): Character {
+
+        if (type == 'gnome') {
             return new Gnome(this.camera, this.renderer);
         }
-        if(type=='golem'){
+        if (type == 'golem') {
             return new Golem(this.camera, this.renderer);
         }
 
+        if (type == 'pumpkin') {
+            return new Pumpkin(this.camera, this.renderer);
+        }
 
     }
 
 
+    add(controller: CharacterController) {
+        this.controllers.push(controller);
+        controller.character.on('character:die', () => {
+            this.controllers.splice(this.controllers.indexOf(controller), 1);
+        });
+    }
 
+    animate(delta) {
+        this.controllers.forEach(c => c.animate(delta))
+    }
+
+
+    clickableMeshesNearby(pos) {
+
+        return this.controllers.map(character => character.object)
+
+    }
 
 
 }
