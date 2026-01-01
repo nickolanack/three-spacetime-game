cf9223ecf7895d05726e7b58c1de19675b23c947
nickolanack <nickblackwell82@gmail.com>
2026-01-01 14:00:30 -0800

    add door item

---
+++ src/Door.ts
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+import { PixelMesh } from './PixelMesh';
+import { Item } from './Item';
+export class Door extends Item {
+
+   override scale=1
+
+
+}
